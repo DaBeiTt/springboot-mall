943b2115aecfee234b8baa4275ce137c1bf90530
DabeiTt <aa06220912@gmail.com>
2024-02-03 17:06:18 +0800

    使用 Lombok 設定 Getter & Setter

--- src/main/java/org/hsiaomartin/springbootmall/dto/ProductQueryParams.java
+++ src/main/java/org/hsiaomartin/springbootmall/dto/ProductQueryParams.java
@@ -1,7 +1,11 @@
 package org.hsiaomartin.springbootmall.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hsiaomartin.springbootmall.constant.ProductCategory;
 
+@Getter
+@Setter
 public class ProductQueryParams {
 
     private ProductCategory category;
@@ -10,52 +14,4 @@ public class ProductQueryParams {
     private String sort;
     private Integer limit;
     private Integer offset;
-
-    public Integer getLimit() {
-        return limit;
-    }
-
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
-
-    public Integer getOffset() {
-        return offset;
-    }
-
-    public void setOffset(Integer offset) {
-        this.offset = offset;
-    }
-
-    public String getOrderBy() {
-        return orderBy;
-    }
-
-    public void setOrderBy(String orderBy) {
-        this.orderBy = orderBy;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public ProductCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(ProductCategory category) {
-        this.category = category;
-    }
-
-    public String getSearch() {
-        return search;
-    }
-
-    public void setSearch(String search) {
-        this.search = search;
-    }
 }
